e5f1f2291b5c345059b3bed2add06ad40a5dbc59
miguelortis <pedroarguelles1996@gmail.com>
2022-06-16 17:54:19 -0430

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const servidor = http.createServer(app);
+const dotenv = require('dotenv').config();
+
+//PORT
+const PORT = process.env.PORT || 3000
+
+//Iniciando socket.io
+const Socketio = require("socket.io");
+const io = Socketio(servidor, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+let users = [];
+
+const addUser = (name, socketId) => {
+    !users.some((user) => user.socketId === socketId) &&
+        users.push({ name, socketId });
+};
+
+const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (name) => {
+    return users.find((user) => user.name === name);
+};
+
+io.on("connection", (socket) => {
+    //when ceonnect
+    console.log("a user connected.", socket.id);
+
+    //take name and socketId from user
+    socket.on("connected", (name) => {
+        addUser(name, socket.id);
+        io.emit("getUsers", users);
+    });
+
+    //send and get message
+    socket.on("message", (name, message) => {
+        //io.emit manda el mensaje a todos los clientes conectados al chat
+        console.log(message, socket.id)
+        io.emit("messages", { name, message, senderId: socket.id });
+    });
+
+
+    //when disconnect
+    socket.on("disconnect", () => {
+        removeUser(socket.id);
+        io.emit("getUsers", users);
+        console.log("a user disconnected!", socket.id);
+    });
+});
+
+servidor.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
